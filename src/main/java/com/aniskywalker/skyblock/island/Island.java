d0743f0b767a5b5041cb683d9a79debb14e319ba
20zinnm <meyerzinn@gmail.com>
2016-09-04 18:21:40 -0500

    All these gitignore templates are too restricting.

---
+++ src/main/java/com/aniskywalker/skyblock/island/Island.java
@@ -0,0 +1,7 @@
+package com.aniskywalker.skyblock.island;
+
+public class Island {
+
+
+
+}
---
+++ src/main/java/com/aniskywalker/skyblock/island/member/Member.java
@@ -0,0 +1,9 @@
+package com.aniskywalker.skyblock.island.member;
+
+import java.util.UUID;
+
+public class Member {
+
+    UUID uuid;
+
+}
